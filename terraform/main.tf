70204b2ed08e31bfc818b161645f7e8f0ee53af8
Om Mandwade <146607354+0ye0m@users.noreply.github.com>
2026-04-09 16:18:12 +0530

    Update Terraform configuration for resource names

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstateom12345"   # change if name taken
+    storage_account_name = "tfstateom12345"   # keep as is (already created)
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "shruti-terraform.tfstate"
   }
 
   required_providers {
@@ -26,15 +26,15 @@ resource "random_id" "rand" {
   byte_length = 4
 }
 
-# Resource Group
+# Resource Group (Shruti)
 resource "azurerm_resource_group" "demo" {
-  name     = "rg-om-demo-prod"
+  name     = "shruti-ta2-rg"
   location = "East Asia"
 }
 
-# Storage Account
+# Storage Account (Shruti)
 resource "azurerm_storage_account" "demo_sa" {
-  name                     = "omstorage${random_id.rand.hex}"
+  name                     = "shrutitf${random_id.rand.hex}"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
