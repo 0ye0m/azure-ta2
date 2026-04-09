18a4efe634ffbae8b71740e7850e00fe0f31225b
0ye0m <mandwadeom@gmail.com>
2026-04-09 15:59:52 +0530

    initialized

---
+++ terraform/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstateom12345"   # change if name taken
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Random for unique storage name
+resource "random_id" "rand" {
+  byte_length = 4
+}
+
+# Resource Group
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-om-demo-prod"
+  location = "East Asia"
+}
+
+# Storage Account
+resource "azurerm_storage_account" "demo_sa" {
+  name                     = "omstorage${random_id.rand.hex}"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Static Web App
+resource "azurerm_static_web_app" "demo_swa" {
+  name                = "om-static-demo-prod"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = "East Asia"
+  sku_tier            = "Free"
+  sku_size            = "Free"
+}
